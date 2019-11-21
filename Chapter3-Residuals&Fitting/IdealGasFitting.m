ef52df49163d88f725245f91c16444b76374aaf6
Hannah Holt <haho3703@colorado.edu>
2019-11-21 13:17:22 -0700

    correction to chi2 test on ideal gas fit

--- Chapter3-Residuals&Fitting/IdealGasFitting.m
+++ Chapter3-Residuals&Fitting/IdealGasFitting.m
@@ -58,7 +58,7 @@ Z_mat = repmat(Z, [1, 72, 36]);
 Z_mat = permute(Z_mat, [2 3 1]);
 
 % sample every 2 hrs
-skip = 5;
+skip = 1;
 % [Temp, den, mbar] in each column for every time step
 globe_avg = zeros(length(modeltime)/skip, 3);
 ind = 1;
@@ -89,20 +89,21 @@ for t_ind = [1:skip:length(modeltime)]
     % now find global mean of Temperature, mbar and den at ~ 400 km
     % CHANGES ----- KEEP rho and mbar together before averaging
 
-    globe_avg(ind,1) = mean(Tnx(:,:,z_want), 'all');      % take mean over lon and lat (i.e. rows and cols)
-    globe_avg(ind,2) = mean(denx(:,:,z_want), 'all');
-    globe_avg(ind,3) = mean(mbarx(:,:,z_want), 'all');
+    globe_avg(ind,1) = GlobeAreaAvg(1./Tnx(:,:,z_want), lon, lat);      % take mean over lon and lat (i.e. rows and cols)
+    globe_avg(ind,2) = GlobeAreaAvg(denx(:,:,z_want), lon, lat);
+    globe_avg(ind,3) = GlobeAreaAvg((denx(:,:,z_want)./mbarx(:,:,z_want)), lon, lat);
     ind = ind+1;
    
 end
 
 %%
 % Calculate fits and residuals of Ideal Gas equation
+tst = 1;
+tstp = 2160;
 
-x = 1./globe_avg(:,1);
-y1 = globe_avg(:,2);
-
-y2 = globe_avg(:,2)./globe_avg(:,3);
+x = globe_avg(tst:tstp,1);         % 1/Temperature
+y1 = globe_avg(tst:tstp,2);        % density
+y2 = globe_avg(tst:tstp,3);        % rho/mbar
 
 [p1, s1] = polyfit(x, y1, 1);
 [p2, s2] = polyfit(x, y2, 1);
@@ -115,58 +116,62 @@ thisfig = figure('units', 'normalized', 'outerposition',[0 1 .6 1]);
 subplot(221)
 plot(x, y1, 'r.'); hold on;
 plot(x, y1_fit, 'k-');
-plot(x, y1_fit+std1, 'k--', x, y1_fit-std1, 'k--')
+% plot(x, y1_fit+std1, 'k--', x, y1_fit-std1, 'k--')
 xlabel('Global 1/T average [1/K]');
 ylabel('\rho [kg/m^3]');
 legend('Data', ['y = ', sprintf('%.3e', p1(1)), 'x + ',  sprintf('%.2f', p1(2))], '+/- 1\sigma', 'location', 'best' )
-title('Global \rho Average');
+title('Global \rho Average Before Storm');
 grid on;
 
 subplot(222);
 plot(x, y2, 'b.'); hold on;
 plot(x, y2_fit, 'k-');
-plot(x, y2_fit+std2, 'k--', x, y2_fit-std2, 'k--');
+% plot(x, y2_fit+std2, 'k--', x, y2_fit-std2, 'k--');
 legend('Data', ['y = ', sprintf('%.3e', p2(1)), 'x + ',  sprintf('%.2f', p2(2))], '+/- 1\sigma', 'location', 'best' )
 xlabel('Global Average of 1/T [1/K]');
 ylabel('\rho / mbar');
-title('Global Average of \rho / mbar')
+title('Global Average of \rho / mbar Before Storm')
 grid on;
 
 subplot(223);
 num_bins = round(sqrt(length(res1)));
-xmin = min(res1);
-xmax = max(res1);
-binspace = linspace(xmin, xmax, num_bins);
-h1 = histogram(res1, binspace);
+xmin = -2.5E-20;
+xmax = -xmin;
+binspace = linspace(min(res1), max(res1), num_bins);
+h1 = histogram(res1, binspace); hold on;
+xvals = linspace(xmin, xmax, num_bins);
+n_syn = normpdf(xvals, mean(res1), std(res1));
+n_syn = n_syn / sum(n_syn);                     % normalize to a total of one
+n_syn = sum(h1.Values) * n_syn;               % scale to data meas
+plot(xvals, n_syn)
 title('Normalized Postfit Residuals');
-% xlim([xmin xmax])
+xlim([xmin xmax])
 grid on;
 
 subplot(224);
-xmin = min(res2);
-xmax = max(res2);
-binspace = linspace(xmin, xmax, num_bins);
-h2 = histogram(res2, binspace);
+xmin = -1E-19;
+xmax = -xmin;
+binspace = linspace(min(res2), max(res2), num_bins);
+h2 = histogram(res2, binspace); hold on;
+xvals = linspace(xmin, xmax, num_bins);
+n_syn = normpdf(xvals, mean(res2), std(res2));
+n_syn = n_syn / sum(n_syn);                     % normalize to a total of one
+n_syn = sum(h2.Values) * n_syn;               % scale to data meas
+plot(xvals, n_syn)
 title('Normalized Postfit Residuals');
-% xlim([xmin xmax]);
+xlim([xmin xmax]);
 grid on;
 
 saveas(thisfig, [FigFolder, 'IdealGasFits.png'])
 
-% perform chi2 test on residuals
-bin_mid1 = h1.BinEdges(1:end-1) + h1.BinWidth;
-bin_mid2 = h2.BinEdges(1:end-1) + h2.BinWidth;
+%% perform chi2 test on residuals
 
-n_syn1 = normpdf(bin_mid1, mean(res1), std(res1));
-n_syn1 = (n_syn1 ./ sum(n_syn1)) .* sum(h1.Values);
+alpha = 0.05;
+ConfLvl = 1-alpha;
+chi2crit = chi2inv(ConfLvl, 38)
 
-n_syn2 = normpdf(bin_mid2, mean(res2), std(res2));
-n_syn2 = (n_syn2 ./ sum(n_syn2)) .* sum(h2.Values);
-
-%%
-chicalc2 = sum((h2.Values - n_syn2).^2 ./n_syn2 )
-chi2crit = chi2inv(0.95, num_bins -(1 + 2))
-
+[test1, p1, stats1] = chi2gof(res1, 'NBins', h1.NumBins)
+[test2, p2, stats2] = chi2gof(res2, 'NBins', h2.NumBins)
 %%
 
 
